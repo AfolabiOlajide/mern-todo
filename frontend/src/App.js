965eac7318ce830c2c203b11955b834cf0547fbc
Afolabi Olajide Samuel <samolajide20@gmail.com>
2023-03-19 20:50:53 +0100

    worked on login and register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,31 @@
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+import { Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home';
+import EditProfile from './pages/EditProfile';
+import Auth from './pages/Auth';
+import PrivateRoutes from './components/PrivateRoutes';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
+    <>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          style: {
+            fontSize: '1.8rem',
+          },
+        }}
+      />
+      <Routes>
+        <Route element={<PrivateRoutes />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/edit-profile" element={<EditProfile />} />
+        </Route>
+        <Route path='/auth' element={<Auth />} />
+      </Routes>
+    </>
   );
 }
 
